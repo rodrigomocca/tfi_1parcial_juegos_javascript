8754edab9946bc549a8eef39172f53378208ac13
Rodrigo Mocca <rodrigo.mocca@hotmail.com>
2019-09-17 21:10:52 -0300

    Memotest

--- memotest/index.js
+++ memotest/index.js
@@ -1,17 +1,24 @@
 var imagenes = [
     "imagen1.png",
+    "imagen1.png",
+    "imagen2.png",
     "imagen2.png"
 ];
+
+function limpiar(){
+    location.reload();
+}
 
 window.onload = function () {
 
     var selectedId = "";
     var selected = "";
-
+    imagenes = imagenes.sort(function () { return 0.5 - Math.random() });
     var tbl = document.getElementById("tblMain");
     if (tbl != null) {
         for (i = 0; i < tbl.rows.length; i++) {
             for (j = 0; j < tbl.rows[i].cells.length; j++) {
+                tbl.rows[i].cells[j].firstElementChild.src = imagenes.pop();
                 tbl.rows[i].cells[j].onclick = function () {
                     cellClick(this);
                 }
